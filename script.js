832e03f4297013335dd545db2713bd0f9fe59a23
Aryanvadaliya <128956837+Aryanvadaliya@users.noreply.github.com>
2023-10-16 18:21:21 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -22,20 +22,52 @@
 
 
 
-const inputImg = document.getElementById('imgInput')
-const img = document.getElementById('img')
+// const inputImg = document.getElementById('imgInput')
+// const img = document.getElementById('img')
 
-function getImg(event){
+// function getImg(event){
 
-     const file = event.target.files[0]; // 0 = get the first file
+//      const file = event.target.files[0]; // 0 = get the first file
 
-     // console.log(file);
+//      // console.log(file);
 
-     let url = window.URL.createObjectURL(file);
+//      let url = window.URL.createObjectURL(file);
 
-     // console.log(url)
+//      // console.log(url)
 
-     img.src = url
-}
+//      img.src = url
+// }
 
-inputImg?.addEventListener('change', getImg)
+// inputImg?.addEventListener('change', getImg)
+
+
+
+
+const video = document.getElementById('video');
+      const canvas = document.getElementById('canvas');
+      const snap = document.getElementById('snap');
+      const constraints = {
+        video: {
+          facingMode: 'environment'
+        }
+      };
+
+      async function init() {
+        try {
+          const stream = await navigator.mediaDevices.getUserMedia(constraints);
+          video.srcObject = stream;
+        } catch (err) {
+          console.error(err);
+        }
+      }
+
+      function takeSnapshot() {
+        const context = canvas.getContext('2d');
+        context.drawImage(video, 0, 0, canvas.width, canvas.height);
+      }
+
+      init();
+
+      snap.addEventListener('click', () => {
+        takeSnapshot();
+      });
